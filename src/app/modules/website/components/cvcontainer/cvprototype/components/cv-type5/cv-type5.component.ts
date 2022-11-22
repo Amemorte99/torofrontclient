723697fe0735ae929b93188ad08a50b0169536fd
princepatrice <princepatriceesmc@gmail.com>
2022-11-22 23:11:24 +0000

    new cv5

--- src/app/modules/website/components/cvcontainer/cvprototype/components/cv-type5/cv-type5.component.ts
+++ src/app/modules/website/components/cvcontainer/cvprototype/components/cv-type5/cv-type5.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgxPrintElementService } from 'ngx-print-element';
 import { AccountService } from '../../../account/account.service';
 
 @Component({
@@ -9,7 +10,7 @@ import { AccountService } from '../../../account/account.service';
 export class CvType5Component implements OnInit {
 
 
-  constructor(public accountService : AccountService) { }
+  constructor(public accountService : AccountService,public print: NgxPrintElementService) { }
 
   ngOnInit(): void {
   }
@@ -22,4 +23,14 @@ export class CvType5Component implements OnInit {
     this.accountService.cvInfo =cvData;
   }
 
+
+  public config2 = {
+    printMode: 'template-popup', // template
+    popupProperties: 'toolbar=yes,scrollbars=yes,resizable=yes,top=0,left=0,fullscreen=yes',
+    pageTitle: '',
+    stylesheets: [{ rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/bootstrap@4.5.0/dist/css/bootstrap.min.css' },{ rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css' }],
+    StyleSheet: "cv-type1.component.css",
+    styles: ['td { border: 1px solid black; color: green; }', 'table { border: 1px solid black; color: red }', 'header, table, footer { margin: auto; text-align: center; }']
+  }
+ 
 }
