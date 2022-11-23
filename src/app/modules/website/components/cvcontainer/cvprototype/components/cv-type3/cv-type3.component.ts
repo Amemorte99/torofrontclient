95c14a84751ed3ec9bf424397f0bc6d39ba03053
princepatrice <princepatriceesmc@gmail.com>
2022-11-23 13:20:45 +0000

    oui

--- src/app/modules/website/components/cvcontainer/cvprototype/components/cv-type3/cv-type3.component.ts
+++ src/app/modules/website/components/cvcontainer/cvprototype/components/cv-type3/cv-type3.component.ts
@@ -19,5 +19,13 @@ export class CvType3Component implements OnInit {
   public set cvInfo(cvData : any){
     this.accountService.cvInfo =cvData;
   }
+  public config2 = {
+    printMode: 'template-popup', // template
+    popupProperties: 'toolbar=yes,scrollbars=yes,resizable=yes,top=0,left=0,fullscreen=yes',
+    pageTitle: '',
+    stylesheets: [{ rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/bootstrap@4.5.0/dist/css/bootstrap.min.css' }],
+    StyleSheet: "cv-type1.component.css",
+    styles: ['td { border: 1px solid black; color: green; }', 'table { border: 1px solid black; color: red }', 'header, table, footer { margin: auto; text-align: center; }']
+  }
 
 }
